cfe8b7b0afaee10311974e894af8f116f08a4ac9
RousseOrtiz <ri.ortizguzman@ugto.mx>
2022-06-11 04:59:52 -0500

    cambios

--- app/vistas/productoVista.php
+++ app/vistas/productoVista.php
@@ -1,12 +1,33 @@
 <?php include_once("encabezado.php"); 
 print "<h2 class='text-center'>".$datos["subtitulo"]."</h2>";
-//Curso 
+print "<img src='".RUTA."img/".$datos["data"]["imagen"]."' class='rounded float-right'/>";
+
+//Curso
 if($datos["data"]["tipo"]==1){
-    print "Descripción: ".$datos["data"]["descripcion"];
+    print "<h4>Descripción:</h4>";
+    print "<p>".html_entity_decode($datos["data"]["descripcion"])."</p>";
+  
+    print "<h4>¿A quién va dirigido?:</h4>";
+    print "<p>".$datos["data"]["publico"]."</p>";
+  
+    print "<h4>Objetivos:</h4>";
+    print "<p>".$datos["data"]["objetivos"]."</p>";
+  
+    print "<h4>Precio (MXN):</h4>";
+    print "<p>$".number_format($datos["data"]["precio"],2)."</p>";
+  
+    print "<h4>¿Qué es necesario?:</h4>";
+    print "<p>".$datos["data"]["necesario"]."</p>";
   } else if($datos["data"]["tipo"]==2){
-    print "Talla: ".$datos["data"]["talla"];
+    print "<h4>Talla:</h4>";
+    print "<p>".$datos["data"]["talla"]."</p>";
+    
+    print "<h4>Precio (MXN):</h4>";
+    print "<p>$".number_format($datos["data"]["precio"],2)."</p>";
+
+    print "<h4>Descripción:</h4>";
+    print "<p>".html_entity_decode($datos["data"]["descripcion"])."</p>";
   }
-
-
-print "<a href='".RUTA."tienda' class='btn btn-success'/>Regresar</a>";
-include_once("piepagina.php"); ?>
+  
+  print "<a href='".RUTA."tienda' class='btn btn-success'/>Regresa</a>";
+  include_once("piepagina.php"); ?>
