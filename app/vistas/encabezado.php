f08da1d1b6f0210c7894c49d8a57d8f33d3cc68d
RousseOrtiz <ri.ortizguzman@ugto.mx>
2022-06-11 13:24:30 -0500

    icono agregado

--- app/vistas/encabezado.php
+++ app/vistas/encabezado.php
@@ -7,6 +7,8 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+</head>
 </head>
 <body>
   <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
@@ -53,14 +55,16 @@
       //
       print "<ul class='nav navbar-nav navbar-right'>";
       //
-      //
-      ?>
-      <form action="<?php print RUTA; ?>buscar/producto" class="form-inline"  method="POST">
-        <input type="text" name="buscar" id="buscar" class="form-control" size="20" placeholder="Buscar un producto">
-        <button type="submit" class="btn btn-dark">ir</button>
-      </form>
-      <?php
-      print "</ul>";
+     //
+     print "<li class='nav-item'>";
+     print "</li>";
+     print "</ul>";
+     ?>
+     <form action="<?php print RUTA; ?>buscar/producto" class="form-inline"  method="POST">
+       <input type="text" name="buscar" id="buscar" class="form-control" size="20" placeholder="Buscar un producto">
+       <button type="submit" class="btn btn-light"><i class="fas fa-search"></i></button>
+     </form>
+     <?php
     }
     if(isset($datos["admon"])){
       if($datos["admon"]){
